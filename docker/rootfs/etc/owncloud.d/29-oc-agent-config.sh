a9256b9d5e4963e8e2bc9f86748877324367c665
Jingyu <jingyu.niu@gmail.com>
2018-04-23 14:28:43 +0800

    Update startup scripts

--- docker/rootfs/etc/owncloud.d/29-oc-agent-config.sh
+++ docker/rootfs/etc/owncloud.d/29-oc-agent-config.sh
@@ -9,16 +9,17 @@ fi
 if [[ ! -f ${ELA_OC_AGENT_CONFIG_FILE} ]]
 then
   echo "Copying Elastos oc-agent config file..."
-  cp /etc/elaoc/elaoc-agent.conf ${ELA_OC_AGENT_CONFIG_FILE}
+  cp /etc/elaoc/elaoc-agent.conf.org ${ELA_OC_AGENT_CONFIG_FILE}
   DIR=${ELA_OC_AGENT_DIR//\//\\/}
   sed -i "s/datadir = data/datadir=${DIR}/g" ${ELA_OC_AGENT_CONFIG_FILE}
 
   SHASUM=`echo -n ${ELA_OC_AGENT_SECRET} | shasum -a 256`
   SECRET=${SHASUM::-3}
   sed -i "s/secret_hello=5efa94085cb2977f90ddd1a671c70aa651cd8a2b62183c9cc647628795b12092/secret_hello=${SECRET}/g" ${ELA_OC_AGENT_CONFIG_FILE}
-
-  mv /etc/elaoc/elaoc-agent.conf /etc/elaoc/elaoc-agent.conf.org
-  ln -s ${ELA_OC_AGENT_CONFIG_FILE} /etc/elaoc/elaoc-agent.conf
 fi
+
+echo "Prepare Elastos oc-agent config file..."
+rm -f /etc/elaoc/elaoc-agent.conf
+ln -s ${ELA_OC_AGENT_CONFIG_FILE} /etc/elaoc/elaoc-agent.conf
 
 true
